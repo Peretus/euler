cfb931e9074fd5390d94da42f71205d5f27a2455
MakerSquare Student <students@themakersquare.com>
2013-11-17 17:25:27 -0600

    Initial commit with first 5 problem solutions

---
+++ Currency_problem_31.rb
@@ -0,0 +1,27 @@
+# coins = [1,2,5,10,20,50,100,200]
+
+# total = []
+
+# i = 0
+
+# coins.each do |coin|
+# 	n = 0
+# 	n ==  200 ? total << n : n;
+# 	until n > 200 do
+# 		puts "#{n} is the current state of n"
+# 		n = n + coin
+# 		end
+# 		i = i+1
+# 	end
+# end
+
+# puts total
+
+
+
+
+
+
+
+
+
--- even_fibonacci_numbers.rb
+++ even_fibonacci_numbers.rb
@@ -1,13 +1,42 @@
 
-array = []
+array = [0,1]
 
-(1...4_000).each do |i|
-	 a = (i - 1)
-	 b = (i -2)
+# # (1...4_000).each do |i|
+# # 	 a = (i - 1)
+# # 	 b = (i -2)
 
-	array << (a+b)
+# # 	array << (a+b)
 
+# # end
+
+
+# # print array[-1]
+
+
+
+
+
+array = [0,1]
+
+ until (array[-1]+array[-2]) > 4_000_000 do
+	array << array[-1]+array[-2]
 end
 
+puts array
+puts "--------------"
 
-print array[-1]
+answer = array.inject(0) { |result, num| num.even? ? result+num : result }
+
+puts answer
+
+# # answer = 0
+# # array.each do |item|
+# # 	if item.even?
+# # 		answer+= item
+# # 	end
+# # end
+
+
+# puts answer
+
+
